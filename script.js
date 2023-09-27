5b96fe3afbede1563a11948037fd7a4e8362be8a
Abir12918 <banikabir1234@gmail.com>
2023-09-26 22:31:10 -0400

    added addColumn and removeColumn

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ function addRow() {
     // Get a reference to the grid table
     var gridTable = document.getElementById('gridTable');
 
-    // Create a new row
-    var newRow = gridTable.insertRow();
-
-    // Create a cell for the new row
-    var cell = newRow.insertCell();
-    cell.className = 'cell';
-
+    // Create a new row if there are no rows
+    if (gridTable.rows.length === 0) {
+        var newRow = gridTable.insertRow();
+        var cell = newRow.insertCell();
+        cell.className = 'cell';
+    } else {
+        // Clone the first row to create a new row
+        var firstRow = gridTable.rows[0];
+        var newRow = firstRow.cloneNode(true);
+        gridTable.appendChild(newRow);
+    }
 }
 
 // Function to remove the most recent row from the grid table
@@ -18,9 +22,42 @@ function removeRow() {
     var gridTable = document.getElementById('gridTable');
 
     // Check if there are any rows to remove
-    if (gridTable.rows.length > 1) {
+    if (gridTable.rows.length > 0) {
         // Remove the last row from the table
         gridTable.deleteRow(-1);
+    }
+}
+
+// Function to add a new column to the grid table
+function addColumn() {
+    // Get a reference to the grid table
+    var gridTable = document.getElementById('gridTable');
+
+    // Create a new cell in each row if there are no rows
+    if (gridTable.rows.length === 0) {
+        var newRow = gridTable.insertRow();
+        var cell = newRow.insertCell();
+        cell.className = 'cell';
+    } else {
+        // Otherwise, add a new cell to each existing row
+        for (var i = 0; i < gridTable.rows.length; i++) {
+            var cell = gridTable.rows[i].insertCell();
+            cell.className = 'cell';
+        }
+    }
+}
+
+// Function to remove the most recent column from the grid table
+function removeColumn() {
+    // Get a reference to the grid table
+    var gridTable = document.getElementById('gridTable');
+
+    // Check if there are any cells in the first row
+    if (gridTable.rows.length > 0 && gridTable.rows[0].cells.length > 0) {
+        // Remove the last cell from each row
+        for (var i = 0; i < gridTable.rows.length; i++) {
+            gridTable.rows[i].deleteCell(-1);
+        }
     }
 }
 
@@ -31,3 +68,11 @@ addRowButton.addEventListener("click", addRow);
 // Add event listener for the "Remove Row" button
 var removeRowButton = document.getElementById("removeRowButton");
 removeRowButton.addEventListener("click", removeRow);
+
+// Add event listener for the "Add Column" button
+var addColumnButton = document.getElementById("addColumnButton");
+addColumnButton.addEventListener("click", addColumn);
+
+// Add event listener for the "Remove Column" button
+var removeColumnButton = document.getElementById("removeColumnButton");
+removeColumnButton.addEventListener("click", removeColumn);
